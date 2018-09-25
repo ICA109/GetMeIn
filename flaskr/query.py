c0b3e486e1fb3c87aaa8f1a1ba59afb696ea0950
Tony <t.yin1@outlook.com>
2018-09-24 23:06:29 -0400

    added testing print statements for email content

--- flaskr/query.py
+++ flaskr/query.py
@@ -1,5 +1,9 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 import smtplib
 
 
@@ -11,8 +15,6 @@ def check_for_updates(posts):
         course_contact = post['body']
         course_term_raw = course_contact.split(',')[0]
         course_list_and_info.append((course_title, course_contact))
-
-        # course_title = 'COMP-330'
         course_term = '000000'
 
         if '2018' in course_term_raw:
@@ -23,12 +25,21 @@ def check_for_updates(posts):
         url = "https://vsb.mcgill.ca/vsb/criteria.jsp?access=0&lang=en&tip=1&page=results&scratch=0&term={1}" \
               "&sort=none&filters=iiiiiiiii&bbs=&ds=&cams=Distance_Downtown_Macdonald_Off-Campus&locs=any&isrts=&" \
               "course_0_0={0}" \
-              "&sa_0_0=&cs_0_0=--201901_9182--&cpn_0_0=&csn_0_0=&ca_0_0=&dropdown_0_0=al&ig_0_0=" \
+              "&sa_0_0=&cs_0_0=--{1}_9182--&cpn_0_0=&csn_0_0=&ca_0_0=&dropdown_0_0=al&ig_0_0=" \
               "0&rq_0_0=".format(course_title, course_term)
 
         url1a = url
         browser = webdriver.PhantomJS()
         browser.get(url1a)
+        delay = 3
+
+        try:
+            my_elem = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.CLASS_NAME, 'seatText')))
+            # print("Page loaded.")
+
+        except TimeoutException:
+            print("Warning taking too long to load. Possible issue with request.")
+
         html = browser.page_source
         soup = BeautifulSoup(html, 'lxml')
         open_seats_section = soup.find_all("span", "seatText")
@@ -37,6 +48,9 @@ def check_for_updates(posts):
         if len(open_seats_section) > 0:
             num_seats_available = open_seats_section[0].text
 
-        print(num_seats_available)
+        # print(num_seats_available)
+        print('-----Sample Email Start-----')
+        print('{0} in {1} has {2} open seats available now'.format(course_title, course_term, num_seats_available))
+        print('-----Sample Email End-----')
 
-    print(course_list_and_info)
+    # print(course_list_and_info)
