2094068a51b81f47271d837a09e03677a8029ddf
Tony <t.yin1@outlook.com>
2018-09-24 21:40:21 -0400

    generic test cases

--- flaskr/query.py
+++ flaskr/query.py
@@ -8,26 +8,34 @@ def check_for_updates(posts):
     for post in posts:
         course_title = post['title']
         course_contact = post['body']
+        course_term_raw = course_contact.split(',')[0]
         course_list_and_info.append((course_title, course_contact))
 
+        # course_title = 'COMP-330'
+        course_term = '000000'
+
+        if '2018' in course_term_raw:
+            course_term = '201809'
+        elif '2019' in course_term_raw:
+            course_term = '201901'
+
+        url = "https://vsb.mcgill.ca/vsb/criteria.jsp?access=0&lang=en&tip=1&page=results&scratch=0&term={1}" \
+              "&sort=none&filters=iiiiiiiii&bbs=&ds=&cams=Distance_Downtown_Macdonald_Off-Campus&locs=any&isrts=&" \
+              "course_0_0={0}" \
+              "&sa_0_0=&cs_0_0=--201901_9182--&cpn_0_0=&csn_0_0=&ca_0_0=&dropdown_0_0=al&ig_0_0=" \
+              "0&rq_0_0=".format(course_title, course_term)
+
+        url1a = url
+        browser = webdriver.PhantomJS()
+        browser.get(url1a)
+        html = browser.page_source
+        soup = BeautifulSoup(html, 'lxml')
+        open_seats_section = soup.find_all("span", "seatText")
+        num_seats_available = 0
+
+        if len(open_seats_section) > 0:
+            num_seats_available = open_seats_section[0].text
+
+        print(num_seats_available)
+
     print(course_list_and_info)
-
-    course_title = 'COMP-520'
-
-    url = "https://vsb.mcgill.ca/vsb/criteria.jsp?access=0&lang=en&tip=1&page=results" \
-          "&scratch=0&term=201901&sort=none&filters=iiiiiiiii&bbs=&ds=&cams=" \
-          "Distance_Downtown_Macdonald_Off-Campus&locs=any&isrts=&course_0_0={0}&sa_0_0=" \
-          "&cs_0_0=--201901_9182--&cpn_0_0=&csn_0_0=&ca_0_0=&dropdown_0_0=al&ig_0_0=0&rq_0_0=".format(course_title)
-
-    url1a = url
-    browser = webdriver.PhantomJS()
-    browser.get(url1a)
-    html = browser.page_source
-    soup = BeautifulSoup(html, 'lxml')
-    open_seats_section = soup.find_all("span", "seatText")
-    num_seats_available = 0
-
-    if len(open_seats_section) > 0:
-        num_seats_available = open_seats_section[0].text
-
-    print(num_seats_available)
