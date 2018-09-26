289b8b0e5dafc1c36e3058d0051f88ac5f313b14
Tony <t.yin1@outlook.com>
2018-09-26 13:28:25 -0400

    pre-ag1

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from celery import Celery
 
+
 app = Flask(__name__)
 app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
 app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
