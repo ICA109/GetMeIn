0a1dc232c20427de2cc05dd4627ecc211c72b18c
Tony <t.yin1@outlook.com>
2018-09-26 20:01:26 -0400

    standalone for sqlite

--- refresh.py
+++ refresh.py
@@ -10,7 +10,10 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import requests
-from flaskr.db import get_db
+
+import sqlite3
+from sqlite3 import Error
+
 
 import celery_config
 # configuration
@@ -18,76 +21,76 @@ app = Celery('refresh', broker='redis://localhost:6379/0', include=['flaskr.blog
 app.config_from_object(celery_config)
 
 
+def create_connection(db_file):
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+
+    return None
 
 
 
 
 
-@app.task
-def fetch_url(url):
-    resp = requests.get(url)
-    print(resp.status_code)
 
 @app.task
 def batch_query():
-    with app.app_context():
-        db = get_db()
-        posts = db.execute(
-            'SELECT p.id, title, body, created, author_id, username'
-            ' FROM post p JOIN user u ON p.author_id = u.id'
-            ' ORDER BY created DESC'
-        ).fetchall()
+    try:
+        conn = sqlite3.connect('instance.flaskr')
+        return conn
+    except Error as e:
+        print(e)
 
-        course_list_and_info = []
+    cur = conn.cursor()
+    posts = cur.execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' ORDER BY created DESC'
+    ).fetchall()
 
-        for post in posts:
-            course_title = post['title']
-            course_contact = post['body']
-            course_term_raw = course_contact.split(',')[0]
-            course_list_and_info.append((course_title, course_contact))
-            course_term = '000000'
+    course_list_and_info = []
 
-            if '2018' in course_term_raw:
-                course_term = '201809'
-            elif '2019' in course_term_raw:
-                course_term = '201901'
+    for post in posts:
+        course_title = post['title']
+        course_contact = post['body']
+        course_term_raw = course_contact.split(',')[0]
+        course_list_and_info.append((course_title, course_contact))
+        course_term = '000000'
 
-            url = "https://vsb.mcgill.ca/vsb/criteria.jsp?access=0&lang=en&tip=1&page=results&scratch=0&term={1}" \
-                  "&sort=none&filters=iiiiiiiii&bbs=&ds=&cams=Distance_Downtown_Macdonald_Off-Campus&locs=any&isrts=&" \
-                  "course_0_0={0}" \
-                  "&sa_0_0=&cs_0_0=--{1}_9182--&cpn_0_0=&csn_0_0=&ca_0_0=&dropdown_0_0=al&ig_0_0=" \
-                  "0&rq_0_0=".format(course_title, course_term)
+        if '2018' in course_term_raw:
+            course_term = '201809'
+        elif '2019' in course_term_raw:
+            course_term = '201901'
 
-            url1a = url
-            browser = webdriver.PhantomJS()
-            browser.get(url1a)
-            delay = 3
+        url = "https://vsb.mcgill.ca/vsb/criteria.jsp?access=0&lang=en&tip=1&page=results&scratch=0&term={1}" \
+              "&sort=none&filters=iiiiiiiii&bbs=&ds=&cams=Distance_Downtown_Macdonald_Off-Campus&locs=any&isrts=&" \
+              "course_0_0={0}" \
+              "&sa_0_0=&cs_0_0=--{1}_9182--&cpn_0_0=&csn_0_0=&ca_0_0=&dropdown_0_0=al&ig_0_0=" \
+              "0&rq_0_0=".format(course_title, course_term)
 
-            try:
-                my_elem = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.CLASS_NAME, 'seatText')))
-                # print("Page loaded.")
+        url1a = url
+        browser = webdriver.PhantomJS()
+        browser.get(url1a)
+        delay = 3
 
-            except TimeoutException:
-                print("Warning taking too long to load. Possible issue with request.")
+        try:
+            my_elem = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.CLASS_NAME, 'seatText')))
+            # print("Page loaded.")
 
-            html = browser.page_source
-            soup = BeautifulSoup(html, 'lxml')
-            open_seats_section = soup.find_all("span", "seatText")
-            num_seats_available = 0
+        except TimeoutException:
+            print("Warning taking too long to load. Possible issue with request.")
 
-            if len(open_seats_section) > 0:
-                num_seats_available = open_seats_section[0].text
+        html = browser.page_source
+        soup = BeautifulSoup(html, 'lxml')
+        open_seats_section = soup.find_all("span", "seatText")
+        num_seats_available = 0
 
-            # print(num_seats_available)
-            print('-----Sample Email Start-----')
-            print('{0} in {1} has {2} open seats available now'.format(course_title, course_term, num_seats_available))
-            print('-----Sample Email End-----')
+        if len(open_seats_section) > 0:
+            num_seats_available = open_seats_section[0].text
 
-@app.task
-def func(urls):
-    for url in urls:
-        fetch_url.delay(url)
-
-
-if __name__ == "__main__":
-    func(["http://google.com", "https://facebook.com", "https://twitter.com", "https://alexa.com"])
+        # print(num_seats_available)
+        print('-----Sample Email Start-----')
+        print('{0} in {1} has {2} open seats available now'.format(course_title, course_term, num_seats_available))
+        print('-----Sample Email End-----')
